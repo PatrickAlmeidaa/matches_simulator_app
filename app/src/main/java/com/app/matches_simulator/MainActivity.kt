f93347d58a06ffeeeb7254a4d8ef30439fe05fed
PatrickAlmeidaa <patrickalmeida0000@gmail.com>
2022-07-23 16:05:13 -0300

    modificação na palheta de cores, implementação do View Binding e criação do ícone

--- app/src/main/java/com/app/matches_simulator/MainActivity.kt
+++ app/src/main/java/com/app/matches_simulator/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.app.matches_simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.matches_simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /*metodo de acesso aos componentes de um layout sem View Binding:
+            setContentView(R.layout.activity_main)
+            val tvHello = findViewById<TextView>(R.id.tvHello)
+        */
+
+        /*utiliza do View Binding para facilitar o acesso aos componentes definidos via layout XML
+        * tornando as chamadas do método findViewById desnecessarias*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.tvHello.text = "Teste Binding"
     }
 }
